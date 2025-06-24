8b0a4771c9bb60349694338388154a422e36cda6
David Heineman <david@davidheineman.com>
2025-06-23 19:35:13 -0700

    remove legacy launcher

--- simple_grpo/beaker/launch.py
+++ simple_grpo/beaker/launch.py
@@ -12,7 +12,10 @@ from rich.pretty import pprint
 from simple_grpo.beaker.config import make_config
 from simple_grpo.beaker.defaults import get_env_vars, get_mounts
 from simple_grpo.beaker.constants import INTERCONNECT_CLUSTERS, WEKA_CLUSTERS, GCP_CLUSTERS
-
+import beaker as bk
+from simple_grpo.beaker.config import make_config
+from simple_grpo.beaker.defaults import get_env_vars, get_mounts
+from gantry.api import launch_experiment
 
 console = Console()
 
@@ -38,15 +41,15 @@ class BeakerConfig:
     task_name: str = "davidh_task"
     priority: str = "normal"
     preemptible: bool = True
-    pure_docker_mode: bool = True # If false, will cd into os.getcwd()
+    pure_docker_mode: bool = True  # If false, will cd into os.getcwd()
     no_auto_dataset_cache: bool = False
     auto_output_dir_path: str = "/oe-eval-default/davidh/deletable_checkpoint"
-    auto_checkpoint_state_dir: str = (
-        "/oe-eval-default/davidh/deletable_checkpoint_states"
-    )
-    beaker_datasets: List[Dict[str, str]] = field(default_factory=list) # TODO: Add parser from mason.py
-    env: List[Dict[str, str]] = field(default_factory=list) # TODO: Add parser from mason.py
-    secret: List[Dict[str, str]] = field(default_factory=list) # TODO: Add parser from mason.py
+    auto_checkpoint_state_dir: str = "/oe-eval-default/davidh/deletable_checkpoint_states"
+    beaker_datasets: List[Dict[str, str]] = field(
+        default_factory=list
+    )  # TODO: Add parser from mason.py
+    env: List[Dict[str, str]] = field(default_factory=list)  # TODO: Add parser from mason.py
+    secret: List[Dict[str, str]] = field(default_factory=list)  # TODO: Add parser from mason.py
     no_host_networking: bool = False
 
 
@@ -76,7 +79,7 @@ def make_command(cmd: List[str], config: BeakerConfig) -> str:
     for idx in range(len(cmd)):
         if "{" in cmd[idx]:
             cmd[idx] = "'" + cmd[idx] + "'"
-    
+
     setup_cmd = ""
     if not config.pure_docker_mode:
         setup_cmd = f"cd {os.getcwd()} && "
@@ -84,10 +87,7 @@ def make_command(cmd: List[str], config: BeakerConfig) -> str:
     # override accelerate call
     join_cmd = " ".join(cmd)
     if config.num_nodes > 1:
-        if (
-            "--num_processes" not in join_cmd
-            and "accelerate" in join_cmd
-        ):
+        if "--num_processes" not in join_cmd and "accelerate" in join_cmd:
             raise ValueError(
                 "num_processes must be specified in the command for accelerate-based multi-node jobs."
             )
@@ -102,39 +102,75 @@ def make_command(cmd: List[str], config: BeakerConfig) -> str:
             ),
             join_cmd,
         )
-    
+
     cmd = setup_cmd + join_cmd
 
     return cmd
 
 
-def make_task_spec(
-    config: BeakerConfig,
-    full_command: str,
-    i: int,
-    beaker_secrets: str,
-    whoami: str,
-):
+def parse_commands() -> List[List[str]]:
+    """
+    Parse commands separated by '--' into list of command lists.
+
+    E.g.:    launch.py [options] -- cmd1 arg1 -- cmd2 arg2
+    Returns: [["cmd1", "arg1"], ["cmd2", "arg2"]]
+    """
+    if len(sys.argv) < 2:
+        raise ValueError("No command provided. Usage: launch.py [options] -- command")
+
+    try:
+        first_cmd_idx = sys.argv.index("--")
+    except ValueError:
+        raise ValueError("No command separator '--' found. Usage: launch.py [options] -- command")
+
+    # Get everything after first --
+    remaining_args = sys.argv[first_cmd_idx + 1 :]
+
+    if not remaining_args:
+        raise ValueError("No command provided after '--'")
+
+    # Split into separate commands on --
+    commands = []
+    current_cmd = []
+
+    for arg in remaining_args:
+        if arg == "--":
+            if current_cmd:
+                commands.append(current_cmd)
+                current_cmd = []
+        else:
+            current_cmd.append(arg)
+
+    if current_cmd:
+        commands.append(current_cmd)
+
+    if not commands:
+        raise ValueError("No valid commands found")
+
+    return commands
+
+
+def launch_gantry(config: BeakerConfig):
     global_wandb_id = gen_uuid()
 
-    if config.num_nodes > 1 and not all(
-        c in INTERCONNECT_CLUSTERS for c in config.cluster
-    ):        
-        raise ValueError(
-            f"Interconnect clusters are required for multi-node jobs; please only use the following clusters: {INTERCONNECT_CLUSTERS}"
-        )
-    
-    if config.image == "ai2/cuda11.8-cudnn8-dev-ubuntu20.04" and any(
-        c in GCP_CLUSTERS for c in config.cluster
-    ):
-        raise ValueError(
-            "GCP clusters do not have the dev filesystem, please use a proper image"
-        )
+    beaker_client = bk.Beaker.from_env(default_workspace=config.workspace)
 
-    if config.hostname is not None:
-        constraints = bk.Constraints(hostname=config.hostname)
-    else:
-        constraints = bk.Constraints(cluster=config.cluster)
+    # print(beaker_client.workspace.secrets())
+    # beaker_secrets = [secret.name for secret in beaker_client.workspace.secrets()]
+    beaker_secrets = []
+
+    # whoami = beaker_client.account.whoami().name
+
+    whoami = 'davidh'
+
+    commands = parse_commands()
+
+    full_commands = []
+    for command in commands:
+        full_commands += [make_command(command, config)]
+
+    assert len(full_commands) == 1, "only one command supported for now"
+    full_commands = full_commands[0]
 
     env_vars, env_secrets = get_env_vars(
         config.cluster,
@@ -147,126 +183,84 @@ def make_task_spec(
         config.secret,
         config.preemptible,
     )
-    env_vars = env_vars + env_secrets # combine both!
+    env_vars = [f"{var.name}={var.value}" for var in env_vars]
+    env_secrets = [f"{var.name}={var.value}" for var in env_secrets]
 
-    mounts = get_mounts(config.beaker_datasets, config.cluster)
+    # mounts = get_mounts(config.beaker_datasets, config.cluster)
+    # mounts = [
+    #     "oe-adapt-default:/oe-adapt-default"
+    #     "oe-training-default:/oe-training-default"
+    #     "oe-eval-default:/oe-eval-default"
+    # ]
+    mounts = None # TODO: How to mount weka??
+
+    ### TODO: Migrate from legacy launcher ???
+    # pure_docker_mode
+    # no_auto_dataset_cache
+    # auto_output_dir_path
+    # auto_checkpoint_state_dir
     
-    spec = bk.TaskSpec(
-        name=f"{config.task_name}__{i}",
-        image=bk.ImageSource(beaker=config.image),
-        command=["/bin/bash", "-c"],
-        arguments=[full_command],
-        result=bk.ResultSpec(path="/output"),
-        datasets=mounts,
-        context=bk.TaskContext(
-            priority=bk.Priority(config.priority), preemptible=config.preemptible
-        ),
-        constraints=constraints,
-        env_vars=env_vars,
-        resources=bk.TaskResources(gpu_count=config.gpus),
-        replicas=config.num_nodes,
-    )
-    
-    if config.num_nodes > 1:
-        spec.leader_selection = True
-        spec.propagate_failure = True
-        spec.propagate_preemption = True
-    if config.no_host_networking:
-        spec.host_networking = False
-    else:
-        spec.host_networking = True
+    # Launch the experiment
+    launch_experiment( # launch_experiment()
+        args=full_commands.split(' '),
 
-    return spec
-
-
-def parse_commands() -> List[List[str]]:
-    """
-    Parse commands separated by '--' into list of command lists.
-    
-    E.g.:    launch.py [options] -- cmd1 arg1 -- cmd2 arg2
-    Returns: [["cmd1", "arg1"], ["cmd2", "arg2"]]
-    """
-    if len(sys.argv) < 2:
-        raise ValueError("No command provided. Usage: launch.py [options] -- command")
-
-    try:
-        first_cmd_idx = sys.argv.index("--")
-    except ValueError:
-        raise ValueError("No command separator '--' found. Usage: launch.py [options] -- command") 
-
-    # Get everything after first --
-    remaining_args = sys.argv[first_cmd_idx + 1:]
-    
-    if not remaining_args:
-        raise ValueError("No command provided after '--'")
-
-    # Split into separate commands on --
-    commands = []
-    current_cmd = []
-    
-    for arg in remaining_args:
-        if arg == "--":
-            if current_cmd:
-                commands.append(current_cmd)
-                current_cmd = []
-        else:
-            current_cmd.append(arg)
-            
-    if current_cmd:
-        commands.append(current_cmd)
-        
-    if not commands:
-        raise ValueError("No valid commands found")
-        
-    return commands
-
-
-def launch(config: BeakerConfig):
-    beaker_client = bk.Beaker.from_env(default_workspace=config.workspace)
-    beaker_secrets = [secret.name for secret in beaker_client.workspace.secrets()]
-    whoami = beaker_client.account.whoami().name
-
-    commands = parse_commands()
-
-    full_commands = []
-    for command in commands:
-        full_commands += [make_command(command, config)]
-
-    experiment_spec = bk.ExperimentSpec(
-        description=config.description,
-        tasks=[
-            make_task_spec(
-                config,
-                full_command,
-                i,
-                beaker_secrets,
-                whoami,
-            )
-            for i, full_command in enumerate(full_commands)
-        ],
+        workspace=config.workspace,
+        clusters=config.cluster,
         budget=config.budget,
-        retry=bk.RetrySpec(allowed_task_retries=config.max_retries),
+
+        # datasets= # add ability to add this
+
+        name=config.task_name,
+        description=config.description,
+        hostnames=config.hostname,
+        beaker_image=config.image,
+        gpus=config.gpus,
+        preemptible=config.preemptible,
+        retries=config.max_retries,
+        mounts=mounts, # need to fix
+        replicas=config.num_nodes,
+        host_networking=not config.no_host_networking,
+        env_vars=env_vars,
+        env_secrets=env_secrets,
+        yes=True,
+
+        # new stuff
+        # allow_dirty=True,
+        dry_run=False,
+        timeout=99999999 # only way to follow the experiment without canceling
     )
-
-    console.rule(f"[bold pink]Beaker experiment spec: [/bold pink]")
-    pprint(experiment_spec)
-
-    console.rule(f"[bold blue]Commands: [/bold blue]")
-    for cmd in full_commands:
-        console.print(Text(cmd))
-
-    exp = beaker_client.experiment.create(spec=experiment_spec)
-    console.log(f"Launched: https://beaker.org/ex/{exp.id}")
 
 
 def main():
+    # config = make_config(BeakerConfig(
+    #     workspace="ai2/davidh",
+    #     cluster=["ai2/jupiter-cirrascale-2"],
+    #     budget="ai2/oe-eval"
+    # ))
     config = make_config(BeakerConfig(
         workspace="ai2/davidh",
         cluster=["ai2/jupiter-cirrascale-2"],
-        budget="ai2/oe-eval"
+        budget="ai2/oe-eval",
+        # gpus=1
     ))
-    launch(config)
+    launch_gantry(config)
 
 
 if __name__ == "__main__":
     main()
+
+
+"""
+Notes on gantry:
+    - Pro: Gantry has support
+    - Con: Gantry installs deps on every job
+
+I think we should do *both*. Mini mason has most of the defaults we need, gantry allows pulling the github container. We just need to write a connector to this function.
+
+Finally, to make things faster, we could write a simple Dockerfile which pre-installs the dependencies and pushes through git actions (although not necessary I think? saves like 2 min of startup time). Then, at startup gantry will just pull the current codebase.
+
+
+Next: How to do code execution? 
+    - Sets of Dockerfiles should be a must -- This is standard in related work.
+    - Could do (1) pre-built dockerfiles (2) launch execution jobs on beaker (so every tool call spins up a container). Could actually be quick to run maybe... (start with this for now. other options are AWS lambdas, or building your own load balancer)
+"""
