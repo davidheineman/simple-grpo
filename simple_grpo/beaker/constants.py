c561413128b540fcb38f3e694477cb9ebe604322
David Heineman <david@davidheineman.com>
2025-06-23 23:20:13 -0700

    remove ganymede

--- simple_grpo/beaker/constants.py
+++ simple_grpo/beaker/constants.py
@@ -3,7 +3,7 @@ WEKA_CLUSTERS = [
     "ai2/saturn-cirrascale",
     "ai2/neptune-cirrascale",
     "ai2/ceres-cirrascale",
-    "ai2/ganymede-cirrascale",
+    "ai2/titan-cirrascale", # B200
 ]
 GCP_CLUSTERS = ["ai2/augusta-google-1"]
 INTERCONNECT_CLUSTERS = [
